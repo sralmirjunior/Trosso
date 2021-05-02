f8e8687bb2b11087e9f2076112c50f763bc7a56f
Joao <38017770+joaovictor74@users.noreply.github.com>
2021-05-02 17:07:51 -0300

    Convertendo ts para js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue'
 
-const routes: Array<RouteRecordRaw> = [
+const routes = [
   {
     path: '/',
     redirect: '/home'
@@ -13,7 +12,6 @@ const routes: Array<RouteRecordRaw> = [
     component: Home
   }
 ]
-
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
