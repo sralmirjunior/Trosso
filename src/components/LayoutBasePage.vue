8cd98b12ed113fbe68cd6db05a2b4146baca1d63
Joao <38017770+joaovictor74@users.noreply.github.com>
2021-05-03 21:48:25 -0300

    Problema com icones resolvido

--- src/components/LayoutBasePage.vue
+++ src/components/LayoutBasePage.vue
@@ -15,9 +15,18 @@
     </ion-content>
     <ion-footer color="primary">
       <ion-toolbar color="primary">
-        <ion-title @click="openModal" color="light"
-          >Adicionar quadro
-        </ion-title>
+        <ion-grid>
+          <ion-row class="ion-justify-content-evenly ion-align-items-center">
+            <ion-item color="primary" lines="none" @click="openModal">
+              <ion-col size="10">
+                <ion-label>Adicionar quadro </ion-label>
+              </ion-col>
+              <ion-col size="2">
+                <ion-icon :icon="addCircleOutline"></ion-icon>
+              </ion-col>
+            </ion-item>
+          </ion-row>
+        </ion-grid>
       </ion-toolbar>
     </ion-footer>
   </ion-page>
@@ -34,9 +43,15 @@ import {
   IonBackButton,
   IonFooter,
   modalController,
+  IonItem,
+  IonGrid,
+  IonRow,
+  IonCol,
 } from "@ionic/vue";
 
 import ModalAddBoard from "./ModalAddBoard";
+
+import { addCircleOutline } from "ionicons/icons";
 
 export default {
   props: ["pageTitle", "pageBackLink"],
@@ -49,6 +64,10 @@ export default {
     IonButtons,
     IonBackButton,
     IonFooter,
+    IonItem,
+    IonGrid,
+    IonRow,
+    IonCol,
   },
   methods: {
     async openModal() {
@@ -58,6 +77,11 @@ export default {
       return modal.present();
     },
   },
+  setup() {
+    return {
+      addCircleOutline,
+    };
+  },
 };
 </script>
 
@@ -65,21 +89,21 @@ export default {
 ion-title {
   text-transform: uppercase;
   font-size: 30px;
-  display: flex;
-  align-items: center;
-  justify-items: center;
 }
 
 ion-footer ion-toolbar {
   border-radius: 12px 12px 0 0;
 }
 
-ion-footer ion-title {
-  text-align: center;
+ion-footer ion-label {
+  text-transform: uppercase;
   font-size: 16px;
 }
 
-ion-icon {
-  width: 100px;
+ion-footer ion-item ion-icon {
+  width: 32px;
+  height: 30px;
+  margin-right: 14px;
+  margin-left: 0px;
 }
 </style>
